ae38738a400f500ef2a886c74a6302e55cab3d20
Andrew D'Angelo <dangeloandrew@outlook.com>
2024-02-17 16:53:08 -0600

    Change I2C LED to use firmware files

--- ui/beepy/led.go
+++ ui/beepy/led.go
@@ -1,65 +1,50 @@
 package beepy
 
 import (
-	"periph.io/x/conn/v3/i2c"
-	"periph.io/x/conn/v3/i2c/i2creg"
-	"periph.io/x/host/v3"
-)
-
-const (
-	write = 0x80
-
-	power = 0x20
-	red   = 0x21
-	green = 0x22
-	blue  = 0x23
+	"os"
+	"strconv"
 )
 
 type LED struct {
-	bus  i2c.BusCloser
-	chip *i2c.Dev
+	level byte
+	r byte
+	g byte
+	b byte
 }
 
 func NewLED() (*LED, error) {
-	if _, err := host.Init(); err != nil {
-		return nil, err
-	}
-
-	i, err := i2creg.Open("1")
-	if err != nil {
-		return nil, err
-	}
-
-	return &LED{
-		bus:  i,
-		chip: &i2c.Dev{Addr: 0x1F, Bus: i},
-	}, nil
+	return &LED{0, 0, 0, 0}, nil
 }
 
 func (l *LED) Close() error {
-	return l.bus.Close()
+	return nil
 }
 
 func (l *LED) On() error {
-	_, err := l.chip.Write([]byte{power + write, 0x01})
-	return err
+	l.level = 0xff
+	return os.WriteFile("/sys/firmware/beepy/led", []byte(strconv.Itoa(int(l.level)) + "\n"), 0220)
 }
 
 func (l *LED) Off() error {
-	_, err := l.chip.Write([]byte{power + write, 0x00})
-	return err
+	l.level = 0x0
+	return os.WriteFile("/sys/firmware/beepy/led", []byte(strconv.Itoa(int(l.level)) + "\n"), 0220)
 }
 
 func (l *LED) SetColor(r, g, b uint16) error {
-	if _, err := l.chip.Write([]byte{red + write, byte(r)}); err != nil {
+
+	l.r = byte(r)
+	l.g = byte(g)
+	l.b = byte(b)
+
+	if err := os.WriteFile("/sys/firmware/beepy/led_red", []byte(strconv.Itoa(int(l.r)) + "\n"), 0220); err != nil {
 		return err
 	}
 
-	if _, err := l.chip.Write([]byte{green + write, byte(g)}); err != nil {
+	if err := os.WriteFile("/sys/firmware/beepy/led_green", []byte(strconv.Itoa(int(l.g)) + "\n"), 0220); err != nil {
 		return err
 	}
 
-	if _, err := l.chip.Write([]byte{blue + write, byte(b)}); err != nil {
+	if err := os.WriteFile("/sys/firmware/beepy/led_blue", []byte(strconv.Itoa(int(l.b)) + "\n"), 0220); err != nil {
 		return err
 	}
 
@@ -67,27 +52,9 @@ func (l *LED) SetColor(r, g, b uint16) error {
 }
 
 func (l *LED) IsOn() (bool, error) {
-	p := make([]byte, 1)
-	if err := l.chip.Tx([]byte{power}, p); err != nil {
-		return false, err
-	}
-
-	return p[0] > 0, nil
+	return l.level > 0, nil
 }
 
 func (l *LED) Color() ([]byte, error) {
-	r := make([]byte, 1)
-	if err := l.chip.Tx([]byte{red}, r); err != nil {
-		return nil, err
-	}
-	g := make([]byte, 1)
-	if err := l.chip.Tx([]byte{green}, g); err != nil {
-		return nil, err
-	}
-	b := make([]byte, 1)
-	if err := l.chip.Tx([]byte{blue}, b); err != nil {
-		return nil, err
-	}
-
-	return []byte{r[0], g[0], b[0]}, nil
+	return []byte{l.r, l.g, l.b}, nil
 }
